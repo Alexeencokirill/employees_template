9fdeb5774844cc001a44d4c61662b46ce66354e1
Alexeencokirill <97600028+Alexeencokirill@users.noreply.github.com>
2023-11-01 23:15:09 +0100

    mutation

--- my-app/src/components/employees-list-item/employees-list-item.js
+++ my-app/src/components/employees-list-item/employees-list-item.js
@@ -23,7 +23,7 @@ import './employees-list-item.css';
     }
 
     render () {
-      const {name, salary} = this.props;
+      const {name, salary, onDelete} = this.props;
       const {increase, rise} = this.state;
 
       let classNames = 'list-group-item d-flex justify-content-between';
@@ -43,11 +43,15 @@ import './employees-list-item.css';
         defaultValue={salary + '$'}
       />
       <div className="d-flex justify-content-center align-items-center">
-        <button type="button" className="btn-cookie btn-sm" onClick={this.onIncrease}>
+        <button type="button" 
+          className="btn-cookie btn-sm" 
+          onClick={this.onIncrease}>
           <i className="fas fa-cookie"></i>
         </button>
 
-        <button type="button" className="btn-trash btn-sm ">
+        <button type="button" 
+          className="btn-trash btn-sm " 
+          onClick={onDelete}>
           <i className="fas fa-trash"></i>
         </button>
         <i className="fas fa-star"></i>
